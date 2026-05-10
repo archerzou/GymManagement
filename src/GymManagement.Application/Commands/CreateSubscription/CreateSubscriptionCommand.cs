311f29243093214a1fac11c2e3083ab20a7b7d4b
Zou Zhefeng <archer.zou84@gmail.com>
2026-05-11 00:58:36 +1200

    refactor the code and implement CQRS pattern

---
+++ src/GymManagement.Application/Commands/CreateSubscription/CreateSubscriptionCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace GymManagement.Application.Commands.CreateSubscription;
+public record CreateSubscriptionCommand(string SubscriptionType,
+    Guid AdminId) : IRequest<Guid>;
---
+++ src/GymManagement.Application/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace GymManagement.Application.Commands.CreateSubscription;
+public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, Guid>
+{
+    public Task<Guid> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(Guid.NewGuid());
+    }
+}

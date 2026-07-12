ddbc68a4a2167be758d68c1fe5a969ce8160ff1b
Zou Zhefeng <archer.zou84@gmail.com>
2026-07-13 00:05:15 +1200

    complete test for domain and begin to set up application

--- src/GymManagement.Domain/Subscriptions/Subscription.cs
+++ src/GymManagement.Domain/Subscriptions/Subscription.cs
@@ -30,7 +30,7 @@ public class Subscription
 
         if (_gymIds.Count >= _maxGyms)
         {
-            return SubscriptionErrors.CannotHaveMoreGymsThanSubscriptionAllows;
+            return SubscriptionErrors.CannotHaveMoreGymsThanTheSubscriptionAllows;
         }
 
         _gymIds.Add(gym.Id);

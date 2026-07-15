1eb126e5810b62d6bb8a15486c5a9641f4e39c71
Zou Zhefeng <archer.zou84@gmail.com>
2026-07-15 23:29:17 +1200

    setup user domain and entity

---
+++ src/GymManagement.Domain/Common/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+
+namespace GymManagement.Domain.Common.Interfaces;
+
+public interface IPasswordHasher
+{
+    ErrorOr<string> HashPassword(string password);
+    bool IsCorrectPassword(string password, string hash);
+}
